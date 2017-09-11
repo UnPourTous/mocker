cb85ddf683b5bbb546e193e72f95d5c36ca0bddd
erichua <erichua@erichuas-MacMini.local>
2017-09-11 12:51:13 +0800

    rfactor mocker, more extendable

--- src/Mocker.js
+++ src/Mocker.js
@@ -8,50 +8,51 @@ import * as _ from './tools'
  * mock tools base on react type
  */
 export default class Mocker {
+  static mock (schema) {
+    if (Mocker._isPrimaryType(schema)) {
+      return Mocker.mockPrimaryType(schema)
+    } else if (_.isObject(schema)) {
+      return Mocker.mockObject(schema)
+    } else if (_.isArray(schema)) {
+      return Mocker.mockArray(schema)
+    } else {
+      return schema
+    }
+  }
+
   static mockObject (objConfig) {
-    const result = {}
+    let result = {}
     for (let key in objConfig) {
-      let valueType = objConfig[key]
-      if (!valueType) {
-        result[key] = valueType
-      } else {
-        if (Mocker._isValidType(valueType)) {
-          result[key] = Mocker.mockValue(valueType)
-        } else {
-          if (_.isPlainObject(valueType)) {
-            result[key] = Mocker.mockObject(valueType)
-          } else if (_.isArray(valueType)) {
-            if (Mocker._isValidType(valueType[0])) {
-              result[key] = [Mocker.mockValue(valueType[0])]
-            } else {
-              result[key] = [Mocker.mockObject(valueType[0])]
-            }
-          } else {
-            result[key] = valueType
-          }
-        }
-      }
+      result[key] = Mocker.mock(objConfig[key])
     }
     return result
   }
 
-  static mockValue (valueType) {
+  static mockArray (valueType) {
+    let result = []
+    for (let i in valueType) {
+      result.push(Mocker.mock(valueType[i]))
+    }
+    return result
+  }
+
+  static mockPrimaryType (valueType) {
     if (!valueType || !valueType.random) {
       throw new Error('valid type config')
     }
     return valueType.random()
   }
 
-  static _isValidType (valueType) {
+  static _isPrimaryType (valueType) {
     if (!valueType || !_.isFunction(valueType.name)) {
       return false
     }
     return [
-      Types.string().name(),
-      Types.boolean().name(),
-      Types.number().name(),
-      Types.string().name(),
-      Types.enum([]).name()
-    ].indexOf(valueType.name()) !== -1
+        Types.string().name(),
+        Types.boolean().name(),
+        Types.number().name(),
+        Types.string().name(),
+        Types.enum([]).name()
+      ].indexOf(valueType.name()) !== -1
   }
 }
